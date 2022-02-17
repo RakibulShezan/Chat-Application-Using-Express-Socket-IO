b1bc70430346115ebacd923af30f3ddf48b86eb3
RakibulShezan <rakibulshezan97@gmail.com>
2022-02-17 13:40:06 +0600

    error response html page

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/common/errorHanlder");
+
+const app = express();
+dotenv.config();
+
+//database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err));
+
+//request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+//404 not found handler
+app.use(notFoundHandler);
+
+//common error handler
+app.use(errorHandler);
+
+// app listen
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
