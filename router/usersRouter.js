e0b06edf7af20db12b3c5e38a120c58f4070f8cb
Shezan-uxd <rakibulshezan97@gmail.com>
2022-03-04 17:02:00 +0600

    file upload and other field validation backend

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,10 +4,24 @@ const express = require("express");
 const router = express.Router();
 
 //internal imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/common/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/common/users/userValidators");
 
 //users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+//add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
